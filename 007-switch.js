bd06f2c424e097f45da929e3bb4fa395b1dc3010
felipesaldias <38324516+felipesaldias@users.noreply.github.com>
2019-07-05 00:41:33 -0400

    Create 007-switch.js

---
+++ 007-switch.js
@@ -0,0 +1,15 @@
+'use strict'
+var edad = 20;
+switch(edad){
+  case 18:
+    console.log("eres mayor de edad recien");
+    brake;
+  case 17:
+    console.log("eri menor");
+    brake;
+  case >30:
+    console.log("viejo ql");
+    brake;
+  default:
+  brake;
+}
